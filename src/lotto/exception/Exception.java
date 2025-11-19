e0baeb71eeb91c198276a2f0a0855b290f80aa48
82102 <dlgustj0524@naver.com>
2025-11-20 08:59:27 +0900

    Feat: [ERROR] 접두사 붙일 부모클래스 구현

--- src/lotto/exception/Exception.java
+++ src/lotto/exception/Exception.java
@@ -2,6 +2,11 @@ package lotto.exception;
 
 //모든 커스텀 예외의 부모 클래스
 //ErrorCode를 받아 생성자에서 [ERROR] 접두사 붙여서 메시지 설정
-public class Exception {
+public class Exception extends IllegalArgumentException {
+    //static -> 클래스 인스턴스를 생성하지 않고도 접근 가능
+    private static final String EP = "[ERROR]";
 
+    public Exception(ErrorCode ec){
+        super(EP + ec.getMessage());
+    }
 }
